ba78a1beb12882cc4e91e02fcb8f48c5e9867a9e
John David Gaspar <johndavidgaspar1107@gmail.com>
2025-02-09 16:37:31 +0800

    Added frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+
+
+  return (
+    <>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App;
